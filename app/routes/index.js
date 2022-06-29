ad88278fa0174358a3e6aadd9cc23a36ff008d88
Leandro Calvo <lcalvo.intern@copyright.com>
2022-06-29 13:14:23 +0200

    attributes problem

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
-
+const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 export default class IndexRoute extends Route {
-  async model() {
+
+   
+/*     async model() {
     return {
       title: 'Grand Old Mansion',
       owner: 'Veruca Salt',
@@ -16,6 +18,17 @@ export default class IndexRoute extends Route {
       bedrooms: 15,
       image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
       description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-    };
-  }
+    }; 
+  } */
+
+      
+ async model(){   
+
+    let response = await fetch('../api/rentals.json');
+    let parsed = await response.json();
+    console.log(parsed)
+    return parsed; 
+
+    }
+    
 }
